2fb7cf9bafe48ef887ac0d8c90e9125881882ad3
Jay <ja34luv@gmail.com>
2023-03-07 17:49:17 -0800

    add Reward_orTrap class and enum

---
+++ demonGame/src/main/java/group3/demonGame/R_T_Type_Enum.java
@@ -0,0 +1,5 @@
+package group3.demonGame;
+
+public enum R_T_Type_Enum {
+    Regular_reward, Bonus_reward, Trap
+}
---
+++ demonGame/src/main/java/group3/demonGame/Reward_or_Trap.java
@@ -0,0 +1,29 @@
+package group3.demonGame;
+
+public class Reward_or_Trap {
+    private Coordinate position;
+    private R_T_Type rt_type;
+    private int value;
+
+    public R_T_Type Reward_or_Trap(Coordinate position, R_T_Type rt_type, int value) {
+    }
+
+    public Coordinate get_position() {
+    }
+
+    public void set_position(Coordinate position) {
+    }
+
+    public R_T_Type get_rt_type() {
+    }
+
+    public void set_rt_type(R_T_Type rt_type) {
+    }
+
+    public int get_value() {
+    }
+
+    public void set_value(int value) {
+    }
+
+}
