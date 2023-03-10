0fe4d4bfcd1b8f925c9ae4949e8a60e4cf4025f2
Jay <ja34luv@gmail.com>
2023-03-10 10:25:51 -0800

    update class methods and variable types

---
+++ demonGame/src/main/java/group3/demonGame/B_Reward.java
@@ -0,0 +1,20 @@
+package group3.demonGame;
+
+public class B_Reward extends Reward_or_Trap {
+    private int duration;
+
+    public B_Reward(Coordinate position, R_T_Type_Enum rt_type, int value, int duration) {
+        this.position = position;
+        this.rt_type = rt_type;
+        this.value = value;
+        this.duration = duration;
+    }
+
+    public int get_duration() {
+        return this.duration;
+    }
+
+    public void set_duration(int duration) {
+        this.duration = duration;
+    }
+}
--- demonGame/src/main/java/group3/demonGame/Reward_or_Trap.java
+++ demonGame/src/main/java/group3/demonGame/Reward_or_Trap.java
@@ -2,22 +2,29 @@ package group3.demonGame;
 
 public class Reward_or_Trap {
     private Coordinate position;
-    private R_T_Type rt_type;
+    private R_T_Type_Enum rt_type;
     private int value;
 
-    public R_T_Type Reward_or_Trap(Coordinate position, R_T_Type rt_type, int value) {
+    public Reward_or_Trap(Coordinate position, R_T_Type_Enum rt_type, int value) {
+        this.position = position;
+        this.rt_type = rt_type;
+        this.value = value;
     }
 
     public Coordinate get_position() {
+        return this.position;
     }
 
     public void set_position(Coordinate position) {
+        this.position = position;
     }
 
-    public R_T_Type get_rt_type() {
+    public R_T_Type_Enum get_rt_type() {
+        return this.rt_type;
     }
 
-    public void set_rt_type(R_T_Type rt_type) {
+    public void set_rt_type(R_T_Type_Enum rt_type) {
+        this.rt_type = rt_type;
     }
 
     public int get_value() {
