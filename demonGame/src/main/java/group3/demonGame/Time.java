827c76889b569b7b44da47beb06820a770c88605
Elizabeth <myw11@sfu.ca>
2023-03-10 21:11:17 -0800

    finished Time class

--- demonGame/src/main/java/group3/demonGame/Time.java
+++ demonGame/src/main/java/group3/demonGame/Time.java
@@ -1,12 +1,38 @@
 package group3.demonGame;
 
 public class Time {
-	private int start;
+	private long start;
+	private long currentTime;
 	
-	public void startTime() {
+	public Time() {
+		start = System.nanoTime();		
+	}
+
+	public long getStartTime() {
+		return start;
+	}
+	public long getCurrentTime() {
+		return currentTime;
+	}
+
+	public long elaspsedTime(long time) {
+		return time - start;
+	}
+	
+	public String stringTime(long time){
+		StringBuilder timeString = new StringBuilder();
+		int seconds = (int) Math.floor(time / 1000000000);
+		int minutes = (int) Math.floor(seconds / 60);
+		
+		timeString.append(minutes);
+		timeString.append(":");
+		timeString.append(seconds);
+		
+		return timeString.toString();
 		
 	}
-	public void pauseTime() {
-		
-	}
+	
+//	public void pauseTime() {
+//		
+//	}
 }
