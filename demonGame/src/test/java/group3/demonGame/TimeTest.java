7566df57724dde8685e969c95b9e27c7d6575afe
zeyoupeng <zeyoup@sfu.ca>
2023-04-02 21:39:16 -0700

    time test

---
+++ demonGame/src/test/java/group3/demonGame/TimeTest.java
@@ -0,0 +1,41 @@
+package group3.demonGame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TimeTest {
+	private GameManager gm;
+
+    private Time TimeUnderTest;
+
+	@Before
+	public void setUp() {
+		GameManager gm = new GameManager();
+		TimeUnderTest= new Time(gm);
+	}
+
+
+
+	@Test
+	public void testGetMinutes() {
+		int a=TimeUnderTest.getMinutes(60);
+		int b=TimeUnderTest.getMinutes(90);
+		assertEquals( 1, a);
+		assertEquals( 1, b);
+	}
+
+	@Test
+	public void testGetSeconds() {
+		int a=TimeUnderTest.getMinutes(60);
+		int a1=TimeUnderTest.getSeconds();
+		int b=TimeUnderTest.getMinutes(90);
+		int b1=TimeUnderTest.getSeconds();
+		assertEquals( 0, a1);
+		assertEquals( 30, b1);
+	}
+
+	
+
+}
